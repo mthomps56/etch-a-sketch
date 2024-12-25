fe41dae9fe6adf31bf3f8e6a009422f78d916570
mthomps <mattthompson56@gmail.com>
2024-12-25 17:08:12 -0500

    Stylesheet created. Size of Logic created.

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 let squareCount = prompt("Enter # of squares per side.");
+const totalPixels = 960;
+let pixelsPerSide = totalPixels / squareCount;
+pixelsPerSide = Math.round(pixelsPerSide);
 
-const div = document.querySelector('div');
-div.textContent = `You've chosen ${squareCount}`;
+console.log(pixelsPerSide);
+
+const div = document.querySelector('container');
+
